53cb49d8533f5a5311da342fe6d54969c1f22f7d
Bloodsfire <voobsheeee@mail.ru>
2020-11-06 21:29:57 +0700

    Create "HeaderLinks" component

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './Header.css'
 
 import HeaderTitle from "./HeaderTitle";
+import HeaderLinks from "./HeaderLinks";
 
 class Header extends React.Component {
     render() {
         return (
             <header>
                 <HeaderTitle />
+                <HeaderLinks />
             </header>
         );
     }
---
+++ src/components/header/HeaderLinks.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './HeaderLinks.css'
+
+class HeaderLinks extends React.Component {
+    render() {
+        return (
+            <div className="header-links">
+            </div>
+        );
+    }
+}
+
+export default HeaderLinks;
