b60c329f941be438c17f09fd627802418c4a1b2b
Bloodsfire <voobsheeee@mail.ru>
2020-11-09 21:43:11 +0700

    Add "utils.js" in server

---
+++ server/utils.js
@@ -0,0 +1,13 @@
+function setRoutes(app, routes) {
+    routes = Array.isArray(routes) ? routes : [routes];
+
+    routes.forEach(route => {
+        app[route.method.toLowerCase()](route.url, (req, res) => {
+            res.send(route.callback());
+        })
+    })
+}
+
+module.exports = {
+    setRoutes
+};
