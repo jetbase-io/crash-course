2e5b06d7c5f959ca4320362d3fe33ef2fa073c1b
Bloodsfire <voobsheeee@mail.ru>
2020-11-11 14:28:13 +0700

    Use request data in Education component

--- src/components/education/Education.js
+++ src/components/education/Education.js
@@ -2,30 +2,42 @@ import React from 'react';
 import './Education.css'
 
 import EducationCard from "./EducationCard";
+import {getEducation} from "../../actions/requestActions";
 
-const cards = [
-    {title: 'BS in Computer Science', text: 'University of Awesomeness', label: '2006-2010'},
-    {title: 'MS in Computer Science', text: 'University of Awesomeness', label: '2010-2011'},
-    {title: 'Certification in Machine Learning', text: 'Institute of Creativity', label: '2011-2012'},
-    {title: 'Certification in Data Analysis', text: 'Institute of Coolness', label: '2012-2013'},
-    {title: 'Certification in Cyber Security', text: 'Institute of Awesomeness', label: '2013-2014'},
-    {title: 'Certification in Big Data', text: 'Institute of Coolness', label: '2014-2015'}
-];
+class Education extends React.Component {
+    constructor(props) {
+        super(props);
 
-const Education = () => {
-    return (
-        <>
-            <h1 className="section-title">Education</h1>
+        this.state = {
+            education: []
+        }
+    }
 
-            <section className="education">
-                <div className="education__center-line" />
+    async componentDidMount() {
+        const education = await getEducation({});
 
-                <div className="education-cards">
-                    {cards.map(card => <EducationCard title={card.title} text={card.text} label={card.label}/>)}
-                </div>
-            </section>
-        </>
-    );
-};
+        this.setState({
+            education: education?.data || []
+        })
+    }
+
+    render() {
+        const { education } = this.state;
+
+        return (
+            <>
+                <h1 className="section-title">Education</h1>
+
+                <section className="education">
+                    <div className="education__center-line" />
+
+                    <div className="education-cards">
+                        {education.map(card => <EducationCard title={card.title} text={card.text} label={card.label}/>)}
+                    </div>
+                </section>
+            </>
+        )
+    }
+}
 
 export default Education;
