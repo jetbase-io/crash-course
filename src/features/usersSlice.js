3577aa2b7b9d77e84df2eb9311003016c0595ddd
Bloodsfire <voobsheeee@mail.ru>
2020-11-12 18:47:25 +0700

    Create users slice

---
+++ src/features/usersSlice.js
@@ -0,0 +1,43 @@
+import { createSlice, createAsyncThunk, createEntityAdapter } from '@reduxjs/toolkit'
+import { getRequest } from "../services/requestService";
+
+const usersAdapter = createEntityAdapter();
+
+const initialState = usersAdapter.getInitialState({
+    status: 'idle',
+    error: null,
+});
+
+export const fetchUsers = createAsyncThunk('users/fetchUsers', async (query) => {
+    const res = await getRequest('/users', query);
+    return res?.data
+});
+
+const usersSlice = createSlice({
+    name: 'users',
+    initialState,
+    reducers: {},
+    extraReducers: {
+        [fetchUsers.pending]: (state) => {
+            state.status = 'loading'
+        },
+        [fetchUsers.fulfilled]: (state, action) => {
+            state.status = 'succeeded';
+            usersAdapter.upsertMany(state, action.payload)
+        },
+        [fetchUsers.rejected]: (state, action) => {
+            state.status = 'failed';
+            state.error = action.payload
+        },
+    }
+});
+
+export default usersSlice.reducer
+
+export const {
+    selectAll: selectAllUsers,
+    selectById: selectUserById,
+} = usersAdapter.getSelectors((state) => state.users);
+
+
+
